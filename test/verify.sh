d91c9168d864777c70693630d05cde6cd88c0674
Łukasz Korecki <lukasz@korecki.me>
2017-03-30 18:05:23 +0100

    Switch to vagrant for veryfication

--- test/verify.sh
+++ test/verify.sh
@@ -3,17 +3,16 @@
 set -oe pipefail
 
 
-dpkg -i envoy.deb
+dpkg -i /vagrant/envoy.deb
 
-echo 'export ENVOY_CONFIG_PATH=/src/envoy/configs/google_com_proxy.json' > /etc/default/envoy
-echo 'export ENVOY_LOG_LEVEL=debug' >> /etc/default/envoy
-echo 'export ENVOY_SERVICE_CLUSTER=test' >> /etc/default/envoy
-echo 'export ENVOY_SERVICE_NODE=node-1' >> /etc/default/envoy
-echo 'export ENVOY_LOG_FILE=/var/log/envoy.log' >> /etc/default/envoy
-echo  'ulimit -n 32000' >> /etc/default/envoy
+echo 'export ENVOY_CONFIG_PATH="/vagrant/envoy/configs/google_com_proxy.json"' > /etc/default/envoy
+echo 'export ENVOY_LOG_LEVEL="debug"' >> /etc/default/envoy
+echo 'export ENVOY_SERVICE_CLUSTER="test"' >> /etc/default/envoy
+echo 'export ENVOY_SERVICE_NODE="node-1"' >> /etc/default/envoy
+echo 'export ENVOY_LOG_FILE="/var/log/envoy.log"' >> /etc/default/envoy
+# echo  'ulimit -n 32000' >> /etc/default/envoy
 
 
-bash
 
 service envoy restart
 
